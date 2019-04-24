e1119fba95db6d441ebf078b780df5679d4eef28
Vytautas Valaitis <vytautas@valaitis.net>
2019-04-24 18:52:41 +0300

    bugfix

--- old/pearson_correlation_test.py
+++ old/pearson_correlation_test.py
@@ -3,7 +3,7 @@ import glob
 import re
 import numpy as np
 
-dataset_dir = '/opt/datasets/data/simulated_flight_1/valid/'
+dataset_dir = '/opt/datasets/data/simulated_flight_1/test/'
 test_data_dir = dataset_dir + '0/*'
 
 all_files = glob.glob(test_data_dir)
@@ -19,7 +19,8 @@ for filename in all_files:
     file_b = filename
   elif triplet == 2:
     file_c = filename
-
+  elif triplet == 3:
+    #print(filename)
     template = cv2.imread(file_a)
     img_b = cv2.imread(file_b)
     img_c = cv2.imread(file_c)
@@ -28,13 +29,17 @@ for filename in all_files:
     img_b = cv2.cvtColor(img_b, cv2.COLOR_BGR2GRAY)
     img_c = cv2.cvtColor(img_c, cv2.COLOR_BGR2GRAY)
     
+    cv2.normalize(template, template, 0, 255, cv2.NORM_MINMAX)
+    cv2.normalize(img_b, img_b, 0, 255, cv2.NORM_MINMAX)
+    cv2.normalize(img_c, img_c, 0, 255, cv2.NORM_MINMAX)
+
     template = cv2.resize(template, (150, 150))
     img_b = cv2.resize(img_b, (150, 150))
     img_c = cv2.resize(img_c, (150, 150))
 
     res_a = cv2.matchTemplate(img_b, template, cv2.TM_CCORR_NORMED)
     res_b = cv2.matchTemplate(img_c, template, cv2.TM_CCORR_NORMED)
-    
+
     r.append([res_a[0][0], res_b[0][0]])
 
     triplet = 0
@@ -43,9 +48,11 @@ for filename in all_files:
 
 r = np.array(r)
 
+print(r)
+
 a = (r[:, 0] - r[:, 1])
 b = (r[:, 0] - r[:, 1])
 
 print(np.sum(np.array(a) >= 0, axis=0))
-print(np.sum(np.array(b) >= 0, axis=0))
+print(np.sum(np.array(b) < 0, axis=0))
 
